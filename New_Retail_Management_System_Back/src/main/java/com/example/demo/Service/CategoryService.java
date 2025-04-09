51633ebe760466f48341ec9ae97da62bd554847d
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2025-04-09 14:03:38 +0530

    in CategoryController
    code change are doing Sakshi

--- New_Retail_Management_System_Back/src/main/java/com/example/demo/Service/CategoryService.java
+++ New_Retail_Management_System_Back/src/main/java/com/example/demo/Service/CategoryService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Model.Category;
+import com.example.demo.Model.User;
 import com.example.demo.Repository.CategoryRepository;
 
 
@@ -18,6 +19,7 @@ public class CategoryService {
 	{
 		return catRepo.isAddNewCategory(category);
     }
+	
 	
 	public List<Category> getAllCategories() {
 		
