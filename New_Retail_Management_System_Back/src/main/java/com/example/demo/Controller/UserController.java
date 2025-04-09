ba27021f1ac81308a19f35cd97f29757609f926e
mrs.sakshi <133587092+sakshikharchan@users.noreply.github.com>
2025-04-09 12:50:15 +0530

    userservice done
    user service done By Sakshi

--- New_Retail_Management_System_Back/src/main/java/com/example/demo/Controller/UserController.java
+++ New_Retail_Management_System_Back/src/main/java/com/example/demo/Controller/UserController.java
@@ -1,10 +1,52 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserService;
 
 @RestController
 @RequestMapping("/api/User")
 public class UserController {
+	
+	@Autowired
+	UserService useservice ;
+	
+	@PostMapping
+	public String addUser (@RequestBody User user)
+	{
+		boolean b =useservice.isAddNewUser(user);
+		if(b)
+		{
+			return "user Added";
+		}
+		
+		return "User Not added";
+	}
+	
+	
+	@PostMapping("/login")
+	public String login(@RequestBody User user) {
+	    String username = user.getUserName();
+	    String password = user.getPassword();
+	    
+	    User loggedInUser = useservice.login(username, password);
+
+	    if (loggedInUser != null) {
+	        int roleId = loggedInUser.getRoleID();
+	        if (roleId == 1) return "redirect:/admin/dashboard";
+	        else if (roleId == 2) return "redirect:/manager/dashboard";
+	        else if (roleId == 3) return "redirect:/cashier/dashboard";
+	        else return "redirect:/login?error=unknownrole";
+	    } else {
+	        return "redirect:/login?error=invalid";
+	    }
+		
+	}
+
 
 }
